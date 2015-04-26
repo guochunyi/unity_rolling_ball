0e44a8793c69add3a89ae2cb683f1ab345ab9f37
guo_chunyi <chunyi.guo@drecom.co.jp>
2015-04-26 09:56:17 +0900

    完成

---
+++ Roll_a_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;	
+	private Rigidbody rb;
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float Horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (Horizontal, 0.0f, vertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			//Destroy(other.gameObject);
+			other.gameObject.SetActive(false);
+		}
+	}
+}
